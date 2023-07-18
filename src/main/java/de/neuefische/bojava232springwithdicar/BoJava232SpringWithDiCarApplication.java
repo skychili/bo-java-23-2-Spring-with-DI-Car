af6d6b703aa6800d5f74dc520bff1e4aaa48db5f
Sara <sara.krieger2022@gmail.com>
2023-07-18 13:06:53 +0200

    Amend gitignore

---
+++ src/main/java/de/neuefische/bojava232springwithdicar/BoJava232SpringWithDiCarApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.bojava232springwithdicar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoJava232SpringWithDiCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoJava232SpringWithDiCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/neuefische/bojava232springwithdicar/BoJava232SpringWithDiCarApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.bojava232springwithdicar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoJava232SpringWithDiCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
