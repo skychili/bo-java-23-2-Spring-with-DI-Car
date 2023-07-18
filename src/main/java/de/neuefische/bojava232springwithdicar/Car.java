3f5bd2f447cd7370b0ffa0439fe68ac0fcd42876
Sara <sara.krieger2022@gmail.com>
2023-07-18 12:57:36 +0200

    Add car

---
+++ src/main/java/de/neuefische/bojava232springwithdicar/Car.java
@@ -0,0 +1,11 @@
+package de.neuefische.bojava232springwithdicar;
+
+import lombok.Data;
+
+@Data
+public class Car {
+    private String id;
+    private String manufacturer;
+    private int tires;
+    private boolean tuv;
+}
